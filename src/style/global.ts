6b2b1616332590ed31c8e04353db66a6945a7925
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2022-01-17 15:31:25 -0300

    First Commit

---
+++ src/style/global.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --purple: #6C63FF;
+        --purple-light: #D7D4FF;
+
+        --white-200: #F9F9F9;
+        --gray: #555555;
+        --gray-dark: #2A2A2A;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+
+    }
+
+    body {
+        font-family: 'Merriweather';
+        background: var(--purple);
+        height: 100vh;
+    }
+
+`
